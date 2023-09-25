d71b9db34a4b3ee0310529599c90318f23b223bb
Syamsalam <105841108720@student.unismuh.ac.id>
2023-09-25 12:20:40 +0800

    login aman

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: `http://localhost:8000/api`,
+    baseURL: `http://192.168.254.198:8000/api`,
     withCredentials: false,
     headers: {
         Accept: 'application/json',
